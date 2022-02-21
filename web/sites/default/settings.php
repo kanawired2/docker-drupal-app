db1080170618cc8d67b8a3797e25914249be8307
kana_nix <kanawired@gmail.com>
2022-02-21 23:13:09 +0530

    initial commit 2

---
+++ web/sites/default/settings.php
@@ -0,0 +1,13 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'docker_drupal_app',
+  'username' => 'root',
+  'password' => 'admin',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['hash_salt'] = 'iXf_7sXlc626cIVryCjsvB5VuL9ydznVEWG3bd1dsE-dsfKHXENl7pjhT_29T5c_oJFFyr68AQ';
+$settings['config_sync_directory'] = 'sites/default/files/config_LZS-EIE4KNlaSrEPHLwrWncSdhkqpJWzxkClAxSeOma6cBOaWzKrxrLlpRS5aun-jzVabet6rA/sync';
